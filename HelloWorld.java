bc6a565b00b5e75c7b30cd85e593ed1c5151fac4
Mukesh Kumar <65854232+mkumarrangi@users.noreply.github.com>
2025-01-19 17:38:54 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 
         public static void main(String[] args) {
-            /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+            System.out.println("Hello From Java");
         }
     }
 
